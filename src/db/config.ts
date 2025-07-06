f54afa750a5103d62618a0aa7abea05dab59a2cf
nelliejellie <emekaewelike@gmail.com>
2025-07-06 09:54:03 +0100

    fix

--- src/db/config.ts
+++ src/db/config.ts
@@ -4,13 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  process.env.DB_NAME,
-  process.env.DB_HOST
-);
-
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST,
